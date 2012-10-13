7268a8ccd32fe096ea6dccddd3232c1579b624c3
Scott Feeney <scott@oceanbase.org>
2012-10-13 02:25:33 -0500

    Version 2 - May 2009 or earlier

--- swancat.c
+++ swancat.c
@@ -37,6 +37,8 @@ static int ssndsrcs = 0, nsndsrcs = 0;
 
 static int lastnote = SEMIRANGE/2+3; // 3 above A-4 (440 Hz) = C-5, I guess
 
+static float beatlength; // in ms
+
 static const char *fadetypes[] = {"lin", "log", "cos", "logcos"};
 
 // which notes are included in the scale used
@@ -79,6 +81,96 @@ static int stepsfrom(int startnote, int steps)
 	return newnote;
 }
 
+static void install_gen(char *cmd, const size_t buflen,
+	const float freq, const float len, const float amp)
+{
+	int which;
+
+	which = rnd(4);
+	if (which == 0) // sine or tri
+	{
+		snprintf(cmd, buflen, "%s -freq %f -len %f -amp %f",
+			rnd(2) ? "sine" : "tri",
+			freq, len, amp);
+	}
+	else if (which == 1) // square or octagon
+	{
+		snprintf(cmd, buflen, "%s -freq %f -len %f -amp %f",
+			rnd(2) ? "square" : "octagon",
+			freq, len, amp);
+	}
+	else if (which == 2) // saw
+	{
+		// 50-50 of being saw up or saw down
+		snprintf(cmd, buflen, "saw %s -freq %f -len %f -amp %f",
+			rnd(2) ? "-down" : "",
+			freq, len, amp);
+	}
+	else if (which == 3) // filtered white noise
+	{
+		// amplified 30dB because the filtering makes what actually
+		// comes out very quiet. it would probably be difficult to
+		// actually equalize with a sine wave volume-wise, as it's
+		// base frequency-dependent.
+		snprintf(cmd, buflen,
+			"white -len %f -amp %f"
+			"|filter -type bp -center %f -q 100"
+			"|amp -dB +30",
+			len, amp, freq);
+	}
+	else errx(1, "oops, not enough generators");
+}
+
+static void add_effect(char *cmd, const size_t buflen)
+{
+	size_t len;
+	size_t spaceleft;
+	char *p;
+	int which;
+
+	len = strlen(cmd);
+	spaceleft = buflen - len;
+	p = cmd + len;
+
+	which = rnd(3);
+	if (which == 0) // delay
+	{
+		float delaylen, feedback, wetout;
+		wetout = frnd()*100.0;
+		delaylen = frnd()*4500.0+10.0;
+		feedback = frnd()*99.9;
+		snprintf(p, spaceleft,
+			"|delay -len %f -feedback %f -wetout %f",
+			delaylen, feedback, wetout);
+	}
+	else if (which == 1) // compressor
+	{
+		float threshdB, ratio, attack, release;
+		int rms = rnd(2);
+		threshdB = (rms ? -12 : -6) - 30*frnd();
+		// note: comp program is backwards as of this writing
+		// and >1 = expander <1 = compressor
+		// though it should probably be the reverse.
+		ratio = 1.0 + frnd()*12.0;
+		if (rnd(8)) // far more likely to compress than expand
+			ratio = 1.0/ratio;
+		attack = frnd()*100.0+10.0;
+		release = frnd()*1000.0+100.0;
+		snprintf(p, spaceleft, "|comp %s -threshdB %f -ratio %f "
+			"-attack %f -release %f",
+			rms ? "-rms" : "", threshdB, ratio, attack, release);
+	}
+	else if (which == 2) // exponential distort
+	{
+		float exponent;
+		exponent = frnd()*3 + 1.0;
+		if (rnd(2))
+			exponent = 1.0 / exponent;
+		snprintf(p, spaceleft, "|power -exp %f", exponent);
+	}
+	else errx(1, "oops, not enough effects");
+}
+
 static FILE *generate_pipe_source(int maxsamps, int *preplacesamps)
 {
 	char cmdstring[10240];
@@ -91,6 +183,7 @@ static FILE *generate_pipe_source(int maxsamps, int *preplacesamps)
 	int interval;
 	int harmonic;
 	int ix;
+	int numfx;
 	const char *infadetype, *outfadetype;
 	FILE *fp;
 
@@ -130,16 +223,30 @@ static FILE *generate_pipe_source(int maxsamps, int *preplacesamps)
 
 	*preplacesamps = (int)((secs - outfadetime) * RATE);
 
-	// XXX note in the following, with the sine program,
-	// I use a sine length 0.05 sec too long since I think inexactness
+	// XXX note in the following, with each program,
+	// I generate 0.2 sec extra since I think inexactness
 	// here, in terms of length, may be the cause of occasional clicks?
 	// But then the clicks still come so I don't know why.
 
-	snprintf(cmdstring, sizeof cmdstring,
-		"sine -freq %f -len %f -amp %f | pan -angle %f "
-		"| fadef in %s 0 %f | fadef out %s %f %f",
-		sinefreq, (secs+0.05)*1000, amp, panangle,
+	install_gen(cmdstring, sizeof cmdstring, sinefreq, (secs+0.2)*1000,
+		amp);
+
+	numfx = rnd(3)+1;
+	for (ix = 0; ix < numfx; ix++)
+		add_effect(cmdstring, sizeof cmdstring);
+
+	snprintf(cmdstring + strlen(cmdstring),
+		sizeof cmdstring - strlen(cmdstring),
+		"|pan -angle %f"
+		"|fadef in %s 0 %f | fadef out %s %f %f",
+		panangle,
 		infadetype, infadetime, outfadetype, secs - outfadetime, secs);
+
+//	snprintf(cmdstring, sizeof cmdstring,
+//		"sine -freq %f -len %f -amp %f | pan -angle %f "
+//		"| fadef in %s 0 %f | fadef out %s %f %f",
+//		sinefreq, (secs+0.2)*1000, amp, panangle,
+//		infadetype, infadetime, outfadetype, secs - outfadetime, secs);
 	fprintf(stderr, "new sound source: %s\n", cmdstring);//Tgk
 	fp = popen(cmdstring, "r");
 	if (fp == NULL)
@@ -184,10 +291,13 @@ static int read_sndsrc(int num, float *dest)
 	return 1;
 }
 
-static void make_sound(int samplen, int initsnds, int minlen, int maxlen)
+static void make_sound(float bpm, int samplen, int initsnds,
+	int minlen, int maxlen)
 {
 	int ix;
 	int sampsout;
+
+	beatlength = 60 / bpm;
 
 	for (ix = 0; ix < initsnds; ix++)
 		add_sndsrc(minlen, maxlen);
@@ -233,12 +343,13 @@ int main(void)
 
 	get_rate();
 
-	// XXX hardcoded parameters
+	// XXX hardcoded random bounds
 	make_sound(
-		RATE * 300, // 5 minutes
-		4, // initial sndsrcs
-		RATE * 5, // minimum length 5 sec
-		RATE * 62 // max length 62 sec
+		frnd()*110.0+70.0, // 70 to 180 bpm
+		rnd(RATE*421) + RATE*120, // 2 to 9 minutes
+		rnd(5)+3, // 3 to 7 initial sndsrcs
+		rnd(RATE*17) + RATE*4, // minimum length 4-20 sec
+		rnd(RATE*46) + RATE*45 // max length 45-90 sec
 	);
 
 	return 0;
